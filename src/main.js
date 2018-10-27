6df1bcaffb7b277c510a439c7a7dca04b7f49fbb
Letícia Lima Cavalcanti <wmli515@gmail.com>
2018-10-27 12:09:23 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+import Vuetify from "vuetify";
+import VueRouter from "vue-router";
+import Home from "./views/Home";
+import Pets from "./views/Pets";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/pets", component: Pets }
+];
+
+const router = new VueRouter({ routes });
+
+Vue.use(Vuetify);
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  components: { App },
+  template: "<App/>",
+  router
+});
